e55fc4afd9e05ec7428a28c40cc075dc8beaf824
rajesssssh <rajesh0807r@gmail.com>
2025-01-04 23:24:51 +0530

    Update pipeline1.groovy

--- vars/pipeline1.groovy
+++ vars/pipeline1.groovy
@@ -1,11 +1,3 @@
-pipeline {
-    agent { label 'slave2' }
-
-    environment {
-        JAVA_HOME = '/usr/lib/jvm/java-17-openjdk-amd64'
-        MAVEN_HOME = '/usr/share/maven'
-        PATH = "${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${env.PATH}"
-    }
 def check_out() {
     echo 'Checking out code...'
     checkout scm
