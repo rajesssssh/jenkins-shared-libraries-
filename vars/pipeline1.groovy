3757836ebeb545997a5d57a31a85150e9f42b99c
rajesssssh <rajesh0807r@gmail.com>
2025-01-04 20:44:05 +0530

    Update pipeline1.groovy

--- vars/pipeline1.groovy
+++ vars/pipeline1.groovy
@@ -29,13 +29,13 @@ def call() {
   
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
-    if (response == "200") {
+    if (response == "200") 
         echo 'The app is running successfully!'
-     else {
+     else 
         echo "The app failed to start. HTTP response code: ${response}"
         error("The app did not start correctly!")
-    }
-  }    
+    
+    
   
     echo 'Waiting for 2 minutes...'
     sleep(time: 2, unit: 'MINUTES')  // Wait for 5 minutes
